30850a096a1b643a1da0dfde8f4c0176a4f1ddbe
Kadir Goksal <136205319+KGoksal@users.noreply.github.com>
2024-08-28 21:04:43 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 } 
- 
+  
 # Create a Subnet in the VPC  
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
